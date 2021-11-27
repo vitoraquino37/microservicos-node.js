1f4b8b6e988f4151439029cce897059ae8ca2ed2
Vitor <54963219+vitoraquino37@users.noreply.github.com>
2021-11-27 14:05:57 -0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://sqerfamv:iV1lRYEFdSdHofgssK7BJhqwhB2URKQI@kesavan.db.elephantsql.com/sqerfamv';
+const connectionString = '**********@kesavan.db.elephantsql.com/sqerfamv';
 
 const db = new Pool({ connectionString });
 
